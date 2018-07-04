ac54fa05fed3d04a3d4854985f3090069f65ce9e
Md. Aftab Uddin Kajal <kajal.alpha@gmail.com>
2018-07-04 15:47:43 +0600

    repository interface and class added

--- smart-crm.Infrastructure/Data/EfRepository.cs
+++ smart-crm.Infrastructure/Data/EfRepository.cs
@@ -1,11 +1,52 @@
-﻿using System;
+﻿using smart_crm.core.Interfaces;
+using smart_crm.core.SharedKernel;
+using smart_crm.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace smart_crm.Infrastructure.Data
 {
-    public class EfRepository
+    public class EfRepository<T> : IAsyncRepository<T> where T : Entity
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public EfRepository(ApplicationDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+
+            return await _dbContext.Set<T>().SingleOrDefaultAsync(m => m.Id == id);
+        }
+
+        public async Task<List<T>> ListAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
     }
 }
---
+++ smart-crm.core/Interfaces/IAsyncRepository.cs
@@ -0,0 +1,19 @@
+﻿using smart_crm.core.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smart_crm.core.Interfaces
+{
+    public interface IAsyncRepository<T> where T : Entity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<List<T>> ListAllAsync();
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+
+    }
+}
