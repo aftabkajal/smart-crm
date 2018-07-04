ec5ef2c01b67a2e662a2e6b07739f01b74054cb4
Md. Aftab Uddin Kajal <kajal.alpha@gmail.com>
2018-07-04 15:38:05 +0600

    Folder added and shared class added

---
+++ smart-crm.Infrastructure/Data/EfRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace smart_crm.Infrastructure.Data
+{
+    public class EfRepository
+    {
+    }
+}
