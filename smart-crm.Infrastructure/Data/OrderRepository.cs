4153e9490e404fdef80cd9f539b4ced59010b286
Md. Aftab Uddin Kajal <kajal.alpha@gmail.com>
2018-07-08 11:46:24 +0600

    Order Repository added

---
+++ smart-crm.Infrastructure/Data/OrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using smart_crm.core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace smart_crm.Infrastructure.Data
+{
+    public class OrderRepository : EfRepository<Order>
+    {
+        public OrderRepository(): base(ApplicationDbContext.Create()) { }
+    }
+}
